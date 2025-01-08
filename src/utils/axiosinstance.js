3f289c4b0a831767f4afccc57be9a4f212e0547b
Bipul Hajong <bipulh62@gmail.com>
2025-01-09 04:32:08 +0600

    update backend url

--- src/utils/axiosinstance.js
+++ src/utils/axiosinstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 const BASE_URL = import.meta.env.VITE_BASE_URL
 const axiosInstance = axios.create({
-  baseURL: BASE_URL,
+  baseURL: BASE_URL || 'http://147.93.31.52:8000',
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json'
